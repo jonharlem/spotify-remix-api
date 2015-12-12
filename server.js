42f052f2df3114be22ec8f002a969fc160f9439f
Jon Harlem <jtharlem@gmail.com>
2015-12-11 16:01:53 -0800

    added views and node modules

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', 'views/');
+app.use(express.static(__dirname + '/public'));
 
 // Your Server Code Here
+app.get('/', function(req, res) {
+	res.render('index');
+});
 
 var server = app.listen(3000, function () {
   var port = server.address().port;
